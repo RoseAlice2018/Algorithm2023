b3e007dc8a981d10123d52878119f9bfb6ba00e0
RoseAlice2018 <1505243015@qq.com>
2023-09-16 23:40:43 +0800

    [Algorthim] add 0915

---
+++ 202309/0915.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int dp[n+1][n+1];
+        dp[0][0] = 0, dp[0][1] = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = dp[i-1][0] + nums[i];
+        }
+        return max(dp[n-1][0], dp[n-1][1]);
+    }
+};
