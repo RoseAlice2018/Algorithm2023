9711380fe0036b7aae5e25609a9c89c501173caf
RoseAlice2018 <1505243015@qq.com>
2023-12-25 00:54:11 +0800

    [Algorithm] add 1224

---
+++ 202312/1224.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumPerimeter(long long neededApples) {
+        long long n = 1;
+        for(; 2 * n * (n + 1) * (2 * n + 1) < neededApples; ++n);
+        return n * 8;
+    }
+};
